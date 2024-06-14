db3ba06481411bdff1822ebc56414ac02369ef29
Seankihiu <seankihiu45@gmail.com>
2024-06-14 08:18:03 -0700

    files

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,7 +10,7 @@ file {'/var/www/html/index.html':
 content => 'Hello World!'
 }
 exec {'redirect_me':
-command => 'sed -i "24i\rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
 provider => 'shell'
 }
 service {'nginx':
