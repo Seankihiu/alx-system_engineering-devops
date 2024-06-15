a7d3658e8f878af8905f921403847d60eaac5544
Seankihiu <seankihiu45@gmail.com>
2024-06-15 03:45:36 -0700

    files

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# Install flask
-package
-{
-'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
-}
+# install flask from pip3
+
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
