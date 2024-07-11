4add84328504628d3a7b2a263cd6c61b12e8d329
Seankihiu <seankihiu45@gmail.com>
2024-07-11 08:01:09 -0700

    files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
-"""
-
+"""querrying the reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    """a function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit"""
+    if not subreddit or not isinstance(subreddit, str):
+        return 0
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {
+            "User-Agent": "Edge/125.0.2535.79 "
+        }
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
+        if response.status_code == 404:
+            return 0
+        results = response.json().get("data")
+        return results.get("subscribers")
+
+    except Exception:
         return 0
