5f1e9262812a766758c2debdb1d648fea33c48a6
Seankihiu <seankihiu45@gmail.com>
2024-07-11 07:08:17 -0700

    files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     headers = {'user-agent': 'request'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     
-    if response.status_code != 404:
+    if response.status_code != 200:
         return 0
         
     data = response.json().get("data")
